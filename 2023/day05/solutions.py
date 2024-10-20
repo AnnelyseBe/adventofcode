9fb1a8e12be23453e243fecb8493d4c9e1b3e07a
Annelies Claes <annelies.claes@vrt.Be>
2024-10-20 23:21:01 +0200

    day03 partA code cleanup + timing

--- 2023/day05/solutions.py
+++ 2023/day05/solutions.py
@@ -1,5 +1,9 @@
 import re
 from collections import namedtuple
+import time
+
+start_time = time.time()
+
 
 INPUT = './2023/day05/input'
 TEST_INPUT = './2023/day05/input_test_1'
@@ -30,9 +34,8 @@ def calculate_conversion(start_value, conversion_ranges):
     conversion=start_value
     for range in conversion_ranges:
         if (start_value >= range.source_start and start_value < range.source_start + range.range_lenght):
-            conversion = range.dest_start + (start_value - range.source_start)
-            break
-    return conversion
+            return range.dest_start + (start_value - range.source_start)
+    return start_value
 
 def calculate_seed_to_location(seed, ranges):
     temp_value = seed
@@ -40,11 +43,19 @@ def calculate_seed_to_location(seed, ranges):
         temp_value = calculate_conversion(temp_value, conversion_ranges)
     return temp_value
 
+def to_seed_ranges_approach(seeds):
+    complete_seed_list = []
+    for i in range(0, len(seeds), 2):
+        complete_seed_list.extend(range(seeds[i], seeds[i] + seeds[i+1]))
+    return complete_seed_list
+
 
 
 print("=================== part A ===================")
 almanac_paragraphs = transform_input(INPUT)
+
 seeds = extract_seeds(almanac_paragraphs)
+print(len(seeds))
 
 seed_to_soil = extract_ranges(almanac_paragraphs[1])
 soil_to_fertilizer = extract_ranges(almanac_paragraphs[2])
@@ -64,12 +75,31 @@ ranges = [
     humidity_to_location
 ]
 
-locations = []
-
-for seed in seeds:
-    locations.append(calculate_seed_to_location(seed, ranges))
+locations = [calculate_seed_to_location(seed, ranges) for seed in seeds]
 
 print(min(locations))
+
+print("=================== part B ===================")
+
+# dit mag ik echt niet runnen. Te groot en laat mijn computer crashen. Ik las ergens dat je moet terugrekenen. Kleinste locatie (=0) terugrekenen naar seedgetal en dan verifieren of dat in onze seeds zit.
+# dan zou het nog 2 min ofzo duren
+# ik zou denken dat we best de seed opdelen in reeksen van 1000 ofzo en telkens de berekening laten doen -> dan zal het wel super lang duren
+
+# complete_seed_list = to_seed_ranges_approach(seeds) 
+
+# locations_complete_seed_list = []
+
+# for seed in complete_seed_list:
+#     locations_complete_seed_list.append(calculate_seed_to_location(seed, ranges))
+
+# print(min(locations_complete_seed_list))
+
+print("============== calculation time ==============")
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+print(f'Execution time: {execution_time:.2f} seconds')
 
 
 
