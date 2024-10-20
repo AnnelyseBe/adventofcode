37289705fa75734ac4746c33589a4198abab3e89
Annelies Claes <annelies.claes@vrt.Be>
2024-10-20 23:28:04 +0200

    calculation time accuracy

--- 2023/day05/solutions.py
+++ 2023/day05/solutions.py
@@ -99,7 +99,7 @@ print("============== calculation time ==============")
 end_time = time.time()
 execution_time = end_time - start_time
 
-print(f'Execution time: {execution_time:.2f} seconds')
+print(f'Execution time: {execution_time:.4f} seconds')
 
 
 
