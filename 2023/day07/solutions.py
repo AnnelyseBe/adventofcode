920e30c7ee03bb323669117f7f998bd5504195ce
Annelies Claes <annelies.claes@vrt.be>
2024-11-29 21:31:32 +0000

    2023 day07 preparation

---
+++ 2023/day07/solutions.py
@@ -0,0 +1,32 @@
+from utils.execution_timer import ExecutionTimer
+import re
+from collections import namedtuple
+
+INPUT = './2023/day07/input'
+TEST_INPUT = './2023/day07/input_test_1'
+
+
+
+def transform_input(inputlocation):
+    with open(inputlocation) as file:
+
+    
+
+ 
+
+print("=================== part A ===================")
+with ExecutionTimer():
+
+    
+print("=================== part B ===================")
+with ExecutionTimer():
+
+
+
+
+
+
+
+
+
+
