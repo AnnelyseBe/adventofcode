4228e6979a78ae168f4b2b80cfa7ef8b794f7359
Annelies Claes <annelies.claes@vrt.be>
2024-12-04 16:46:30 +0000

    template adjustment

--- 2024/dayXX copy/solutions.py
+++ 2024/dayXX copy/solutions.py
@@ -1,10 +1,17 @@
 from utils.execution_timer import ExecutionTimer
 from collections import namedtuple, Counter
 import numpy as np
+from pathlib import Path
 
-INPUT = './2024/day02/input'
-TEST_INPUT = './2024/day02/input_test'
-TEST_INPUT_B = './2024/day02/input_test_B'
+
+# Define the base directory relative to the current script's location
+BASE_DIR = Path(__file__).parent  # Path to the current script's folder
+
+# Define paths to your input files dynamically
+INPUT = BASE_DIR / 'input'
+TEST_INPUT = BASE_DIR / 'input_test'
+TEST_INPUT_B = BASE_DIR / 'input_test_B'
+
 
 def transform_input(inputlocation):
     with open(inputlocation) as file:
--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 import re
+import numpy as np
 
 
 content= 'ljml_jmljlmllk_lk'
-
-
 input_strings = re.split(r"(_)", content)
 print(input_strings)
+
+my_array = np.arange(1,31).reshape(6,5)
+print(my_array)
+
+my_diagonal = my_array.diagonal(offset=-1)
+print(my_diagonal)
+
+
+my_diagonal = my_array.diagonal(offset=-len(my_array)+1)
+print(my_diagonal)
+
+
